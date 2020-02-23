0f1c4f039480971d00fd797451956a45628257fb
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-23 16:30:27 +0200

    task2

--- flights/serializers.py
+++ flights/serializers.py
@@ -13,3 +13,15 @@ class BookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
+
+
+
+class BookingDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ['flight', 'date','passengers', 'id']
+
+class BookingUpdateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ['passengers', 'date', 'id']
